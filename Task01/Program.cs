23dcd258a0791a981413aa0edef382740cf3e716
SAIDRUKHULLO <saidrukhullo@gmail.com>
2023-02-26 10:31:15 +0600

    Initial coomit1

---
+++ Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// // // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+
+
+
+// Напишите программу, которая на 
+// 1.вход принимает два числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int squareB = numberB * numberB;
+if (numberA == squareB)
+{
+   Console.WriteLine($"Число {numberA} является квадратом {numberB}"); 
+}
+else 
+{
+    Console.WriteLine($"Число {numberA} не является квадратом {numberB}");
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,44 @@
+﻿// // // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+
+
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+
+Console.WriteLine("Введите номер дня недели:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberA == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+else if (numberA == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (numberA == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (numberA == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (numberA == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberA == 7)
+{
+    Console.WriteLine("Воскресенья");
+}
+else 
+{
+    Console.WriteLine("Неверноe число дня недели");
+}
