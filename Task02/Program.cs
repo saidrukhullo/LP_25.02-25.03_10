3e6083e293829c1961d410adfc874f395fa1aa47
SAIDRUKHULLO <saidrukhullo@gmail.com>
2023-03-05 09:57:37 +0600

    Initial coomit7

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,17 +5,17 @@
 // a = -9 b = -3 -> max = -3
 
 
-Console.Write("Введите целое число 1: "); 
-string str1 = Console.ReadLine( ); 
-Console.Write("Введите целое число 2: "); 
-string str2 = Console.ReadLine( );  
-int a = Convert.ToInt32(str1); 
-int b = Convert.ToInt32(str2);  
-if (a > b) 
-{     
-    Console.WriteLine($"Число {a} больше, число {b} меньше"); 
-} 
-else 
-{     
-    Console.WriteLine($"Число {b} больше, число {a} меньше"); 
+Console.Write("Введите целое число 1: ");
+string str1 = Console.ReadLine();
+Console.Write("Введите целое число 2: ");
+string str2 = Console.ReadLine();
+int a = Convert.ToInt32(str1);
+int b = Convert.ToInt32(str2);
+if (a > b)
+{
+    Console.WriteLine($"Число {a} больше, число {b} меньше");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше, число {a} меньше");
 }
---
+++ Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число -> {number}");
+
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+
+// int result = firstDigit * 10 + lastDigit;
+
+// Console.Write($"Результат {result}");
+
+int removeSecondDigit = RemoveSecondDigit(number);
+Console.WriteLine($"Результат = {removeSecondDigit}");
+
+int RemoveSecondDigit(int num) // num = number
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = (num - (firstDigit * 100) - lastDigit) / 10;
+    return result;
+}
