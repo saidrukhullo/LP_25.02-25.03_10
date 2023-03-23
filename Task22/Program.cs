cb1221a45f8826a31c8b5bf4e39b6042c1cc9ae7
SAIDRUKHULLO <saidrukhullo@gmail.com>
2023-03-23 11:25:52 +0600

    Initial coomit14

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -10,10 +10,10 @@ TableSquare(number);
 
 void TableSquare(int num)
 {
-int count = 1;
-while (count <= num)
-{
-Console.WriteLine($" {count,3} {count * count, 5} ");
-count++;
-}
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($" {count,3} {count * count,5} ");
+        count++;
+    }
 }
