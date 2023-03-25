d2935d3fd61b38b5000dba1ecc2e44621ae19e72
SAIDRUKHULLO <saidrukhullo@gmail.com>
2023-03-25 14:41:38 +0600

    Initial coomit18

---
+++ Task67/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      else return n * FactorialRec(n - 1);
+//  }
+
+//  Console.WriteLine(FactorialRec(10)); // 3628800
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+
+Console.WriteLine(sumNumbers);
+
+// 456
+
+int SumNumbers(int num)
+ {
+     if (num == 0) return 0;
+     else return num % 10 + SumNumbers(num/10);
+ }
---
+++ Task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int pow = Power(number1, number2);
+
+Console.WriteLine(pow);
+
+int Power(int num1, int num2)
+{
+    if (num2 == 0) return 1;
+    else return num1 * Power(num1,num2 - 1);
+}
